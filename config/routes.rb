333435c0dc8ed099f001de07594801eb0b8de422
Haithem Ben Hadj Khalifa <haithembhk1998@gmail.com>
2019-03-07 21:05:39 +0100

    added about page with picture

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
  # get 'home/index'
 root 'home#index'
 get 'home/contact'
+get 'about/me'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
